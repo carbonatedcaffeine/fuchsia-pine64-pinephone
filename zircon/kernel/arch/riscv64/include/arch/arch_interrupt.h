694a4c1c486350b981bd7fa5584cba433428c63b
Florent Revest <revest@google.com>
2020-08-09 21:25:42 +0200

    [zircon] Add interrupts enabling/disabling support to riscv64
    Change-Id: I952be7f77220feae5c5efb8d459a4a2b3ff6d169

--- zircon/kernel/arch/riscv64/include/arch/arch_interrupt.h
+++ zircon/kernel/arch/riscv64/include/arch/arch_interrupt.h
@@ -10,6 +10,7 @@
 #include <zircon/compiler.h>
 
 #include <arch/riscv64/interrupt.h>
+#include <arch/riscv64.h>
 
 // Note: still pulled in from some C code, remove when the last C code is gone.
 __BEGIN_CDECLS
@@ -18,18 +19,12 @@ typedef bool interrupt_saved_state_t;
 
 __WARN_UNUSED_RESULT
 static inline interrupt_saved_state_t arch_interrupt_save(void) {
-  interrupt_saved_state_t state = false;
-  if (!arch_ints_disabled()) {
-    state = true;
-    arch_disable_ints();
-  }
-  return state;
+  /* disable interrupts by clearing the MIE bit while atomically saving the old state */
+  return riscv_csr_read_clear(RISCV_CSR_XSTATUS, RISCV_CSR_XSTATUS_IE) & RISCV_CSR_XSTATUS_IE;
 }
 
 static inline void arch_interrupt_restore(interrupt_saved_state_t old_state) {
-  if (old_state) {
-    arch_enable_ints();
-  }
+  riscv_csr_set(RISCV_CSR_XSTATUS, old_state ? RISCV_CSR_XSTATUS_IE : 0);
 }
 
 __END_CDECLS
