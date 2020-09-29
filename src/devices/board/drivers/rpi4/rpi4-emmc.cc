gobftald/fuchsia-rpi

[raspberry][zircon][boot-shim]   minimal (smp) boot for rpi4
only pdev devices (uart, gic, psci, timer) are working
psci needs BL31 (ARM Trusted Firmware) installed on SD card

platfrom bus starts a dummy rpi4 board driver
gpio, i2c, sd, eth etc. drivers are bound but don't work yet
they are only templates copied from vim3

but anyway Raspberry Pi 4 boots into kernel shell with all cpus

Change-Id: If2701bb65161679ad4149fc35b9bf136c858fdd8

src/devices/board/drivers/rpi4/rpi4-emmc.cc
@@ -1,0 +1,137 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// for the time being it's only a template copy from vim3
+
+#include <ddk/binding.h>
+#include <ddk/debug.h>
+#include <ddk/metadata.h>
+#include <ddk/metadata/gpt.h>
+#include <ddk/platform-defs.h>
+#include <ddk/protocol/sdmmc.h>
+#include <hw/reg.h>
+#include <soc/aml-common/aml-sd-emmc.h>
+#include <soc/aml-a311d/a311d-gpio.h>
+#include <soc/aml-a311d/a311d-hw.h>
+
+#include "rpi4.h"
+
+namespace rpi4 {
+#define BIT_MASK(start, count) (((1 << (count)) - 1) << (start))
+#define SET_BITS(dest, start, count, value) \
+  ((dest & ~BIT_MASK(start, count)) | (((value) << (start)) & BIT_MASK(start, count)))
+
+static const pbus_mmio_t emmc_mmios[] = {{
+    .base = A311D_EMMC_C_BASE,
+    .length = A311D_EMMC_C_LENGTH,
+}};
+
+static const pbus_irq_t emmc_irqs[] = {
+    {
+        .irq = A311D_SD_EMMC_C_IRQ,
+        .mode = ZX_INTERRUPT_MODE_EDGE_HIGH,
+    },
+};
+
+static const pbus_bti_t emmc_btis[] = {
+    {
+        .iommu_index = 0,
+        .bti_id = BTI_EMMC,
+    },
+};
+
+static aml_sd_emmc_config_t config = {
+    .supports_dma = true,
+    .min_freq = 400000,
+    .max_freq = 120000000,
+    .version_3 = true,
+    .prefs = SDMMC_HOST_PREFS_DISABLE_HS400,
+};
+static const guid_map_t guid_map[] = {
+    {"zircon-a", GUID_ZIRCON_A_VALUE},
+    {"zircon-b", GUID_ZIRCON_B_VALUE},
+    {"zircon-r", GUID_ZIRCON_R_VALUE},
+    {"fvm", GUID_FVM_VALUE},
+};
+static const pbus_metadata_t emmc_metadata[] = {
+    {
+        .type = DEVICE_METADATA_EMMC_CONFIG,
+        .data_buffer = &config,
+        .data_size = sizeof(config),
+    },
+    {
+        .type = DEVICE_METADATA_GUID_MAP,
+        .data_buffer = guid_map,
+        .data_size = sizeof(guid_map),
+    },
+};
+
+static const pbus_boot_metadata_t emmc_boot_metadata[] = {
+    {
+        .zbi_type = DEVICE_METADATA_PARTITION_MAP,
+        .zbi_extra = 0,
+    },
+};
+
+static const zx_bind_inst_t root_match[] = {
+    BI_MATCH(),
+};
+static const zx_bind_inst_t gpio_match[] = {
+    BI_ABORT_IF(NE, BIND_PROTOCOL, ZX_PROTOCOL_GPIO),
+    BI_MATCH_IF(EQ, BIND_GPIO_PIN, A311D_GPIOBOOT(12)),
+};
+static const device_fragment_part_t gpio_fragment[] = {
+    {countof(root_match), root_match},
+    {countof(gpio_match), gpio_match},
+};
+static const device_fragment_t fragments[] = {
+    {countof(gpio_fragment), gpio_fragment},
+};
+
+zx_status_t Rpi4::EmmcInit() {
+  zx_status_t status;
+
+  pbus_dev_t emmc_dev = {};
+  emmc_dev.name = "aml_emmc";
+  emmc_dev.vid = PDEV_VID_AMLOGIC;
+  emmc_dev.pid = PDEV_PID_GENERIC;
+  emmc_dev.did = PDEV_DID_AMLOGIC_SD_EMMC_C;
+  emmc_dev.mmio_list = emmc_mmios;
+  emmc_dev.mmio_count = countof(emmc_mmios);
+  emmc_dev.irq_list = emmc_irqs;
+  emmc_dev.irq_count = countof(emmc_irqs);
+  emmc_dev.bti_list = emmc_btis;
+  emmc_dev.bti_count = countof(emmc_btis);
+  emmc_dev.metadata_list = emmc_metadata;
+  emmc_dev.metadata_count = countof(emmc_metadata);
+  emmc_dev.boot_metadata_list = emmc_boot_metadata;
+  emmc_dev.boot_metadata_count = countof(emmc_boot_metadata);
+
+  // set alternate functions to enable EMMC
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(0), A311D_GPIOBOOT_0_EMMC_D0_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(1), A311D_GPIOBOOT_1_EMMC_D1_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(2), A311D_GPIOBOOT_2_EMMC_D2_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(3), A311D_GPIOBOOT_3_EMMC_D3_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(4), A311D_GPIOBOOT_4_EMMC_D4_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(5), A311D_GPIOBOOT_5_EMMC_D5_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(6), A311D_GPIOBOOT_6_EMMC_D6_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(7), A311D_GPIOBOOT_7_EMMC_D7_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(8), A311D_GPIOBOOT_8_EMMC_CLK_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(10), A311D_GPIOBOOT_10_EMMC_CMD_FN);
+  //gpio_impl_.SetAltFunction(A311D_GPIOBOOT(12), 1);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(13), A311D_GPIOBOOT_13_EMMC_DS_FN);
+
+  gpio_impl_.ConfigOut(A311D_GPIOBOOT(14), 1);
+
+
+
+  status = pbus_.CompositeDeviceAdd(&emmc_dev, fragments, countof(fragments), UINT32_MAX);
+  if (status != ZX_OK) {
+    zxlogf(ERROR, "SdEmmcInit could not add emmc_dev: %d\n", status);
+    return status;
+  }
+
+  return ZX_OK;
+}
+}  // namespace rpi4

src/devices/board/drivers/rpi4/rpi4-sd.cc
@@ -1,0 +1,89 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// for the time being it's only a template copy from vim3
+
+#include <ddk/binding.h>
+#include <ddk/debug.h>
+#include <ddk/metadata.h>
+#include <ddk/platform-defs.h>
+#include <ddk/protocol/sdmmc.h>
+#include <soc/aml-a311d/a311d-gpio.h>
+#include <soc/aml-a311d/a311d-hw.h>
+#include <soc/aml-common/aml-sd-emmc.h>
+
+#include "rpi4.h"
+
+namespace rpi4 {
+
+static const pbus_mmio_t sd_mmios[] = {
+    {
+        .base = A311D_EMMC_B_BASE,
+        .length = A311D_EMMC_B_LENGTH,
+    },
+};
+
+static const pbus_irq_t sd_irqs[] = {
+    {
+        .irq = A311D_SD_EMMC_B_IRQ,
+        .mode = ZX_INTERRUPT_MODE_EDGE_HIGH,
+    },
+};
+
+static const pbus_bti_t sd_btis[] = {
+    {
+        .iommu_index = 0,
+        .bti_id = BTI_SD,
+    },
+};
+
+static aml_sd_emmc_config_t config = {
+    .supports_dma = true,
+    .min_freq = 400'000,
+    .max_freq = 50'000'000,
+    .version_3 = true,
+    .prefs = 0,
+};
+
+static const pbus_metadata_t sd_metadata[] = {
+    {
+        .type = DEVICE_METADATA_EMMC_CONFIG,
+        .data_buffer = &config,
+        .data_size = sizeof(config),
+    },
+};
+
+zx_status_t Rpi4::SdInit() {
+  zx_status_t status;
+
+  pbus_dev_t sd_dev = {};
+  sd_dev.name = "aml_sd";
+  sd_dev.vid = PDEV_VID_AMLOGIC;
+  sd_dev.pid = PDEV_PID_GENERIC;
+  sd_dev.did = PDEV_DID_AMLOGIC_SD_EMMC_B;
+  sd_dev.mmio_list = sd_mmios;
+  sd_dev.mmio_count = countof(sd_mmios);
+  sd_dev.irq_list = sd_irqs;
+  sd_dev.irq_count = countof(sd_irqs);
+  sd_dev.bti_list = sd_btis;
+  sd_dev.bti_count = countof(sd_btis);
+  sd_dev.metadata_list = sd_metadata;
+  sd_dev.metadata_count = countof(sd_metadata);
+
+  gpio_impl_.SetAltFunction(A311D_GPIOC(0), A311D_GPIOC_0_SDCARD_D0_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOC(1), A311D_GPIOC_1_SDCARD_D1_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOC(2), A311D_GPIOC_2_SDCARD_D2_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOC(3), A311D_GPIOC_3_SDCARD_D3_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOC(4), A311D_GPIOC_4_SDCARD_CLK_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOC(5), A311D_GPIOC_5_SDCARD_CMD_FN);
+
+  if ((status = pbus_.CompositeDeviceAdd(&sd_dev, nullptr, 0, UINT32_MAX)) != ZX_OK) {
+    zxlogf(ERROR, "SdInit could not add sd_dev: %d", status);
+    return status;
+  }
+
+  return ZX_OK;
+}
+
+}  // namespace rpi4

src/devices/board/drivers/rpi4/rpi4-sdio.cc
@@ -1,0 +1,105 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// for the time being it's only a template copy from vim3
+
+#include <ddk/binding.h>
+#include <ddk/debug.h>
+#include <ddk/metadata.h>
+#include <ddk/platform-defs.h>
+#include <ddk/protocol/sdmmc.h>
+#include <soc/aml-a311d/a311d-gpio.h>
+#include <soc/aml-a311d/a311d-hw.h>
+#include <soc/aml-common/aml-sd-emmc.h>
+
+#include "rpi4.h"
+
+namespace rpi4 {
+
+static const pbus_mmio_t sdio_mmios[] = {
+    {
+        .base = A311D_EMMC_A_BASE,
+        .length = A311D_EMMC_A_LENGTH,
+    },
+};
+
+static const pbus_irq_t sdio_irqs[] = {
+    {
+        .irq = A311D_SD_EMMC_A_IRQ,
+        .mode = ZX_INTERRUPT_MODE_EDGE_HIGH,
+    },
+};
+
+static const pbus_bti_t sdio_btis[] = {
+    {
+        .iommu_index = 0,
+        .bti_id = BTI_SDIO,
+    },
+};
+
+static aml_sd_emmc_config_t config = {
+    .supports_dma = true,
+    .min_freq = 400'000,
+    .max_freq = 200'000'000,
+    .version_3 = true,
+    .prefs = 0,
+};
+
+static const pbus_metadata_t sdio_metadata[] = {
+    {
+        .type = DEVICE_METADATA_EMMC_CONFIG,
+        .data_buffer = &config,
+        .data_size = sizeof(config),
+    },
+};
+
+static const zx_bind_inst_t root_match[] = {
+    BI_MATCH(),
+};
+static const zx_bind_inst_t wifi_pwren_gpio_match[] = {
+    BI_ABORT_IF(NE, BIND_PROTOCOL, ZX_PROTOCOL_GPIO),
+    BI_MATCH_IF(EQ, BIND_GPIO_PIN, A311D_GPIOX(6)),
+};
+static const device_fragment_part_t wifi_pwren_gpio_fragment[] = {
+    {std::size(root_match), root_match},
+    {std::size(wifi_pwren_gpio_match), wifi_pwren_gpio_match},
+};
+static const device_fragment_t sdio_fragments[] = {
+    {std::size(wifi_pwren_gpio_fragment), wifi_pwren_gpio_fragment},
+};
+
+zx_status_t Rpi4::SdioInit() {
+  zx_status_t status;
+
+  pbus_dev_t sdio_dev = {};
+  sdio_dev.name = "aml_sdio";
+  sdio_dev.vid = PDEV_VID_AMLOGIC;
+  sdio_dev.pid = PDEV_PID_GENERIC;
+  sdio_dev.did = PDEV_DID_AMLOGIC_SD_EMMC_A;
+  sdio_dev.mmio_list = sdio_mmios;
+  sdio_dev.mmio_count = countof(sdio_mmios);
+  sdio_dev.irq_list = sdio_irqs;
+  sdio_dev.irq_count = countof(sdio_irqs);
+  sdio_dev.bti_list = sdio_btis;
+  sdio_dev.bti_count = countof(sdio_btis);
+  sdio_dev.metadata_list = sdio_metadata;
+  sdio_dev.metadata_count = countof(sdio_metadata);
+
+  gpio_impl_.SetAltFunction(A311D_GPIOX(0), A311D_GPIOX_0_SDIO_D0_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOX(1), A311D_GPIOX_1_SDIO_D1_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOX(2), A311D_GPIOX_2_SDIO_D2_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOX(3), A311D_GPIOX_3_SDIO_D3_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOX(4), A311D_GPIOX_4_SDIO_CLK_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOX(5), A311D_GPIOX_5_SDIO_CMD_FN);
+
+  if ((status = pbus_.CompositeDeviceAdd(&sdio_dev, sdio_fragments, countof(sdio_fragments),
+                                         UINT32_MAX)) != ZX_OK) {
+    zxlogf(ERROR, "SdInit could not add sdio_dev: %d", status);
+    return status;
+  }
+
+  return ZX_OK;
+}
+
+}  // namespace rpi4

carbonatedcaffeine/fuchsia-pine64-pinephone

Change-Id: Ie0d03de4099af33ae4bbe949cd5d07823438fbad

src/devices/board/drivers/rpi4/rpi4-emmc.cc
@@ -1,0 +1,137 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// for the time being it's only a template copy from vim3
+
+#include <ddk/binding.h>
+#include <ddk/debug.h>
+#include <ddk/metadata.h>
+#include <ddk/metadata/gpt.h>
+#include <ddk/platform-defs.h>
+#include <ddk/protocol/sdmmc.h>
+#include <hw/reg.h>
+#include <soc/aml-common/aml-sdmmc.h>
+#include <soc/aml-a311d/a311d-gpio.h>
+#include <soc/aml-a311d/a311d-hw.h>
+
+#include "rpi4.h"
+
+namespace rpi4 {
+#define BIT_MASK(start, count) (((1 << (count)) - 1) << (start))
+#define SET_BITS(dest, start, count, value) \
+  ((dest & ~BIT_MASK(start, count)) | (((value) << (start)) & BIT_MASK(start, count)))
+
+static const pbus_mmio_t emmc_mmios[] = {{
+    .base = A311D_EMMC_C_BASE,
+    .length = A311D_EMMC_C_LENGTH,
+}};
+
+static const pbus_irq_t emmc_irqs[] = {
+    {
+        .irq = A311D_SD_EMMC_C_IRQ,
+        .mode = ZX_INTERRUPT_MODE_EDGE_HIGH,
+    },
+};
+
+static const pbus_bti_t emmc_btis[] = {
+    {
+        .iommu_index = 0,
+        .bti_id = BTI_EMMC,
+    },
+};
+
+static aml_sdmmc_config_t config = {
+    .supports_dma = true,
+    .min_freq = 400000,
+    .max_freq = 120000000,
+    .version_3 = true,
+    .prefs = SDMMC_HOST_PREFS_DISABLE_HS400,
+};
+static const guid_map_t guid_map[] = {
+    {"zircon-a", GUID_ZIRCON_A_VALUE},
+    {"zircon-b", GUID_ZIRCON_B_VALUE},
+    {"zircon-r", GUID_ZIRCON_R_VALUE},
+    {"fvm", GUID_FVM_VALUE},
+};
+static const pbus_metadata_t emmc_metadata[] = {
+    {
+        .type = DEVICE_METADATA_EMMC_CONFIG,
+        .data_buffer = &config,
+        .data_size = sizeof(config),
+    },
+    {
+        .type = DEVICE_METADATA_GUID_MAP,
+        .data_buffer = guid_map,
+        .data_size = sizeof(guid_map),
+    },
+};
+
+static const pbus_boot_metadata_t emmc_boot_metadata[] = {
+    {
+        .zbi_type = DEVICE_METADATA_PARTITION_MAP,
+        .zbi_extra = 0,
+    },
+};
+
+static const zx_bind_inst_t root_match[] = {
+    BI_MATCH(),
+};
+static const zx_bind_inst_t gpio_match[] = {
+    BI_ABORT_IF(NE, BIND_PROTOCOL, ZX_PROTOCOL_GPIO),
+    BI_MATCH_IF(EQ, BIND_GPIO_PIN, A311D_GPIOBOOT(12)),
+};
+static const device_fragment_part_t gpio_fragment[] = {
+    {countof(root_match), root_match},
+    {countof(gpio_match), gpio_match},
+};
+static const device_fragment_t fragments[] = {
+    {countof(gpio_fragment), gpio_fragment},
+};
+
+zx_status_t Rpi4::EmmcInit() {
+  zx_status_t status;
+
+  pbus_dev_t emmc_dev = {};
+  emmc_dev.name = "aml_emmc";
+  emmc_dev.vid = PDEV_VID_AMLOGIC;
+  emmc_dev.pid = PDEV_PID_GENERIC;
+  emmc_dev.did = PDEV_DID_AMLOGIC_SDMMC_C;
+  emmc_dev.mmio_list = emmc_mmios;
+  emmc_dev.mmio_count = countof(emmc_mmios);
+  emmc_dev.irq_list = emmc_irqs;
+  emmc_dev.irq_count = countof(emmc_irqs);
+  emmc_dev.bti_list = emmc_btis;
+  emmc_dev.bti_count = countof(emmc_btis);
+  emmc_dev.metadata_list = emmc_metadata;
+  emmc_dev.metadata_count = countof(emmc_metadata);
+  emmc_dev.boot_metadata_list = emmc_boot_metadata;
+  emmc_dev.boot_metadata_count = countof(emmc_boot_metadata);
+
+  // set alternate functions to enable EMMC
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(0), A311D_GPIOBOOT_0_EMMC_D0_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(1), A311D_GPIOBOOT_1_EMMC_D1_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(2), A311D_GPIOBOOT_2_EMMC_D2_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(3), A311D_GPIOBOOT_3_EMMC_D3_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(4), A311D_GPIOBOOT_4_EMMC_D4_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(5), A311D_GPIOBOOT_5_EMMC_D5_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(6), A311D_GPIOBOOT_6_EMMC_D6_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(7), A311D_GPIOBOOT_7_EMMC_D7_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(8), A311D_GPIOBOOT_8_EMMC_CLK_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(10), A311D_GPIOBOOT_10_EMMC_CMD_FN);
+  //gpio_impl_.SetAltFunction(A311D_GPIOBOOT(12), 1);
+  gpio_impl_.SetAltFunction(A311D_GPIOBOOT(13), A311D_GPIOBOOT_13_EMMC_DS_FN);
+
+  gpio_impl_.ConfigOut(A311D_GPIOBOOT(14), 1);
+
+
+
+  status = pbus_.CompositeDeviceAdd(&emmc_dev, fragments, countof(fragments), UINT32_MAX);
+  if (status != ZX_OK) {
+    zxlogf(ERROR, "SdEmmcInit could not add emmc_dev: %d\n", status);
+    return status;
+  }
+
+  return ZX_OK;
+}
+}  // namespace rpi4

src/devices/board/drivers/rpi4/rpi4-sd.cc
@@ -1,0 +1,89 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// for the time being it's only a template copy from vim3
+
+#include <ddk/binding.h>
+#include <ddk/debug.h>
+#include <ddk/metadata.h>
+#include <ddk/platform-defs.h>
+#include <ddk/protocol/sdmmc.h>
+#include <soc/aml-a311d/a311d-gpio.h>
+#include <soc/aml-a311d/a311d-hw.h>
+#include <soc/aml-common/aml-sdmmc.h>
+
+#include "rpi4.h"
+
+namespace rpi4 {
+
+static const pbus_mmio_t sd_mmios[] = {
+    {
+        .base = A311D_EMMC_B_BASE,
+        .length = A311D_EMMC_B_LENGTH,
+    },
+};
+
+static const pbus_irq_t sd_irqs[] = {
+    {
+        .irq = A311D_SD_EMMC_B_IRQ,
+        .mode = ZX_INTERRUPT_MODE_EDGE_HIGH,
+    },
+};
+
+static const pbus_bti_t sd_btis[] = {
+    {
+        .iommu_index = 0,
+        .bti_id = BTI_SD,
+    },
+};
+
+static aml_sdmmc_config_t config = {
+    .supports_dma = true,
+    .min_freq = 400'000,
+    .max_freq = 50'000'000,
+    .version_3 = true,
+    .prefs = 0,
+};
+
+static const pbus_metadata_t sd_metadata[] = {
+    {
+        .type = DEVICE_METADATA_EMMC_CONFIG,
+        .data_buffer = &config,
+        .data_size = sizeof(config),
+    },
+};
+
+zx_status_t Rpi4::SdInit() {
+  zx_status_t status;
+
+  pbus_dev_t sd_dev = {};
+  sd_dev.name = "aml_sd";
+  sd_dev.vid = PDEV_VID_AMLOGIC;
+  sd_dev.pid = PDEV_PID_GENERIC;
+  sd_dev.did = PDEV_DID_AMLOGIC_SDMMC_B;
+  sd_dev.mmio_list = sd_mmios;
+  sd_dev.mmio_count = countof(sd_mmios);
+  sd_dev.irq_list = sd_irqs;
+  sd_dev.irq_count = countof(sd_irqs);
+  sd_dev.bti_list = sd_btis;
+  sd_dev.bti_count = countof(sd_btis);
+  sd_dev.metadata_list = sd_metadata;
+  sd_dev.metadata_count = countof(sd_metadata);
+
+  gpio_impl_.SetAltFunction(A311D_GPIOC(0), A311D_GPIOC_0_SDCARD_D0_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOC(1), A311D_GPIOC_1_SDCARD_D1_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOC(2), A311D_GPIOC_2_SDCARD_D2_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOC(3), A311D_GPIOC_3_SDCARD_D3_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOC(4), A311D_GPIOC_4_SDCARD_CLK_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOC(5), A311D_GPIOC_5_SDCARD_CMD_FN);
+
+  if ((status = pbus_.CompositeDeviceAdd(&sd_dev, nullptr, 0, UINT32_MAX)) != ZX_OK) {
+    zxlogf(ERROR, "SdInit could not add sd_dev: %d", status);
+    return status;
+  }
+
+  return ZX_OK;
+}
+
+}  // namespace rpi4

src/devices/board/drivers/rpi4/rpi4-sdio.cc
@@ -1,0 +1,105 @@
+// Copyright 2020 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// for the time being it's only a template copy from vim3
+
+#include <ddk/binding.h>
+#include <ddk/debug.h>
+#include <ddk/metadata.h>
+#include <ddk/platform-defs.h>
+#include <ddk/protocol/sdmmc.h>
+#include <soc/aml-a311d/a311d-gpio.h>
+#include <soc/aml-a311d/a311d-hw.h>
+#include <soc/aml-common/aml-sdmmc.h>
+
+#include "rpi4.h"
+
+namespace rpi4 {
+
+static const pbus_mmio_t sdio_mmios[] = {
+    {
+        .base = A311D_EMMC_A_BASE,
+        .length = A311D_EMMC_A_LENGTH,
+    },
+};
+
+static const pbus_irq_t sdio_irqs[] = {
+    {
+        .irq = A311D_SD_EMMC_A_IRQ,
+        .mode = ZX_INTERRUPT_MODE_EDGE_HIGH,
+    },
+};
+
+static const pbus_bti_t sdio_btis[] = {
+    {
+        .iommu_index = 0,
+        .bti_id = BTI_SDIO,
+    },
+};
+
+static aml_sdmmc_config_t config = {
+    .supports_dma = true,
+    .min_freq = 400'000,
+    .max_freq = 200'000'000,
+    .version_3 = true,
+    .prefs = 0,
+};
+
+static const pbus_metadata_t sdio_metadata[] = {
+    {
+        .type = DEVICE_METADATA_EMMC_CONFIG,
+        .data_buffer = &config,
+        .data_size = sizeof(config),
+    },
+};
+
+static const zx_bind_inst_t root_match[] = {
+    BI_MATCH(),
+};
+static const zx_bind_inst_t wifi_pwren_gpio_match[] = {
+    BI_ABORT_IF(NE, BIND_PROTOCOL, ZX_PROTOCOL_GPIO),
+    BI_MATCH_IF(EQ, BIND_GPIO_PIN, A311D_GPIOX(6)),
+};
+static const device_fragment_part_t wifi_pwren_gpio_fragment[] = {
+    {std::size(root_match), root_match},
+    {std::size(wifi_pwren_gpio_match), wifi_pwren_gpio_match},
+};
+static const device_fragment_t sdio_fragments[] = {
+    {std::size(wifi_pwren_gpio_fragment), wifi_pwren_gpio_fragment},
+};
+
+zx_status_t Rpi4::SdioInit() {
+  zx_status_t status;
+
+  pbus_dev_t sdio_dev = {};
+  sdio_dev.name = "aml_sdio";
+  sdio_dev.vid = PDEV_VID_AMLOGIC;
+  sdio_dev.pid = PDEV_PID_GENERIC;
+  sdio_dev.did = PDEV_DID_AMLOGIC_SDMMC_A;
+  sdio_dev.mmio_list = sdio_mmios;
+  sdio_dev.mmio_count = countof(sdio_mmios);
+  sdio_dev.irq_list = sdio_irqs;
+  sdio_dev.irq_count = countof(sdio_irqs);
+  sdio_dev.bti_list = sdio_btis;
+  sdio_dev.bti_count = countof(sdio_btis);
+  sdio_dev.metadata_list = sdio_metadata;
+  sdio_dev.metadata_count = countof(sdio_metadata);
+
+  gpio_impl_.SetAltFunction(A311D_GPIOX(0), A311D_GPIOX_0_SDIO_D0_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOX(1), A311D_GPIOX_1_SDIO_D1_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOX(2), A311D_GPIOX_2_SDIO_D2_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOX(3), A311D_GPIOX_3_SDIO_D3_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOX(4), A311D_GPIOX_4_SDIO_CLK_FN);
+  gpio_impl_.SetAltFunction(A311D_GPIOX(5), A311D_GPIOX_5_SDIO_CMD_FN);
+
+  if ((status = pbus_.CompositeDeviceAdd(&sdio_dev, sdio_fragments, countof(sdio_fragments),
+                                         UINT32_MAX)) != ZX_OK) {
+    zxlogf(ERROR, "SdInit could not add sdio_dev: %d", status);
+    return status;
+  }
+
+  return ZX_OK;
+}
+
+}  // namespace rpi4