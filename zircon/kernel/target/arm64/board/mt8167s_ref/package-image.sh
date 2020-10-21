b3b902c10ed592c9f92fe77e13852a67346f647f
Braden Kell <bradenkell@google.com>
2020-10-21 21:57:14 +0000

    DO NOT SUBMIT: Modify package-image script for mt8167 board
    Instructions:
    On the host, run:
    apt-get install u-boot-tools
    fx build
    
    In the device's u-boot shell, run:
    env set mmcboot 'mmc dev ${mmcdev};mmc read ${kerneladdr} 0x2400 0x10000;bootm ${kerneladdr};'
    env save
    fastboot 0
    
    On the host, run:
    fastboot flash boot out/default/zedboot.zbi.signed reboot
    
    Change-Id: I1317962e7778a7d13e7b97ab3164fa6b21ce3591

--- zircon/kernel/target/arm64/board/mt8167s_ref/package-image.sh
+++ zircon/kernel/target/arm64/board/mt8167s_ref/package-image.sh
@@ -12,6 +12,38 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ZIRCON_DIR="${DIR}/../../../../.."
 SCRIPTS_DIR="${ZIRCON_DIR}/scripts"
 
-"${SCRIPTS_DIR}/package-image.sh" -a -b mt8167s_ref \
-    -d "$ZIRCON_DIR/kernel/target/arm64/dtb/dummy-device-tree.dtb" -D append \
-    -g -M 0x40000000 $@
+BOARD=mt8167s_ref
+ZIRCON_BUILD_DIR=
+BOOT_IMG=
+ZIRCON_BOOTIMAGE=
+
+while getopts "B:o:z:" FLAG; do
+    case $FLAG in
+        B) ZIRCON_BUILD_DIR="${OPTARG}";;
+        o) BOOT_IMG="${OPTARG}";;
+        z) ZIRCON_BOOTIMAGE="${OPTARG}";;
+        \?)
+            echo unrecognized option
+            exit 1
+            ;;
+    esac
+done
+
+if [[ -z "${ZIRCON_BUILD_DIR}" ]]; then
+    echo must specify a Zircon build directory
+    HELP
+fi
+
+ROOT_BUILD_DIR="${ZIRCON_BUILD_DIR%%.zircon}"
+
+# boot shim for our board
+BOOT_SHIM="${ROOT_BUILD_DIR}/${BOARD}-boot-shim.bin"
+
+# zircon ZBI image with prepended boot shim
+SHIMMED_ZIRCON_BOOTIMAGE="${ZIRCON_BOOTIMAGE}.shim"
+
+cat "${BOOT_SHIM}" "${ZIRCON_BOOTIMAGE}" > "${SHIMMED_ZIRCON_BOOTIMAGE}"
+
+mkimage -f auto -A arm64 -O linux -T kernel -C none -a 0x40080000 -e 0x40080000 \
+    -d "${SHIMMED_ZIRCON_BOOTIMAGE}" \
+    -b "${ZIRCON_DIR}/kernel/target/arm64/dtb/dummy-device-tree.dtb" "${BOOT_IMG}"
